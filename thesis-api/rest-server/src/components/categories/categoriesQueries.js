3c0b51e18893fad10500eb59be0b63fffe55c258
Daniel Khafi <Dkhaf001@gmail.com>
2018-03-15 16:26:32 -0700

    server skeleton

---
+++ thesis-api/rest-server/src/components/categories/categoriesQueries.js
@@ -0,0 +1,15 @@
+export const Query = async (req, res) => {
+  try {
+    
+  } catch (err) {
+    console.log(err)
+  }
+};
+
+export const Query = async (req, res) => {
+  try {
+
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ thesis-ui/client/src/components/Following/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Following extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div>
+        Hello from Following
+      </div>
+    )
+  }
+}
+
+export default Following
