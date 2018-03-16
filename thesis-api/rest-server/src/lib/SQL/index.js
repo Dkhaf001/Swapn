b1d89af2b1a9a1169ab346dfc77e3c20949b18e0
Edward Roh <edroh90@gmail.com>
2018-03-16 09:42:18 -0700

    fixed syntax.. still have a few bugs

--- thesis-api/rest-server/src/lib/SQL/index.js
+++ thesis-api/rest-server/src/lib/SQL/index.js
@@ -1,27 +1,22 @@
 require('dotenv').config();
 
-import db from '../../config/database';
+import db from '../../config/database/index';
 
-const database = process.env || 'barter' ;
+const database = process.env || 'barter';
 
 //create databases export
 export const createDatabase = async () => {
   try {
-   await db.queryAsync(
-     `CREATE DATABASE ${database}`
-    );
+    await db.queryAsync(`CREATE DATABASE ${database}`);
     console.log('successfully created database', database);
   } catch (err) {
     console.log('error creating database', err);
   }
 };
 
-
 export const dropDatabase = async () => {
   try {
-    await db.queryAsync(
-      `DROP DATABASE IF EXISTS ${database}`
-    );
+    await db.queryAsync(`DROP DATABASE IF EXISTS ${database}`);
     console.log('successfully dropped database ', database);
   } catch (err) {
     console.log('error dropping database ', err);
@@ -30,9 +25,7 @@ export const dropDatabase = async () => {
 
 export const useDatabase = async () => {
   try {
-    await db.queryAsync(
-      `USE IF EXISTS ${database}`
-    );
+    await db.queryAsync(`USE IF EXISTS ${database}`);
     console.log('successfully using database ', database);
   } catch (err) {
     console.log('error using database ', err);
@@ -45,29 +38,26 @@ export const createUserTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS users
       (
-      id SERIAL,
-      email VARCHAR NULL DEFAULT NULL,
-      username VARCHAR(18) NULL DEFAULT NULL,
-      password VARCHAR NULL DEFAULT NULL,
-      location VARCHAR NULL DEFAULT NULL,
-      reputation INTEGER NULL DEFAULT NULL,
-      follower_count INTEGER NULL DEFAULT NULL,
-      following_count INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (id)
+      id SERIAL PRIMARY KEY,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      username VARCHAR(18) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      location VARCHAR(255) NOT NULL,
+      reputation INT,
+      follower_count INT,
+      following_count INT
       )
       `
     );
     console.log('successfully created users table');
   } catch (err) {
-    console.log('error creating users table ', err)
+    console.log('error creating users table ', err);
   }
 };
 
 export const dropUserTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS users`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS users`);
     console.log('successfully dropped users table');
   } catch (err) {
     console.log('error dropping users table ', err);
@@ -80,34 +70,29 @@ export const createPostTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS posts
       (
-      id SERIAL,
-      title VARCHAR NULL DEFAULT NULL,
-      description VARCHAR NULL DEFAULT NULL,
-      condition VARCHAR NULL DEFAULT NULL,
-      location VARCHAR NULL DEFAULT NULL,
-      demand VARCHAR NULL DEFAULT NULL,
-      user_id INTEGER NULL DEFAULT NULL,
-      watch_count INTEGER NULL DEFAULT NULL,
-      view_count INTEGER NULL DEFAULT NULL,
-      offer_count INTEGER NULL DEFAULT NULL,
-      status VARCHAR NULL DEFAULT NULL,
-      PRIMARY KEY (id),
-      CONSTRAINT fk_posts_user_id
-      FOREIGN KEY(user_id) REFERENCES users(id),
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      description VARCHAR(255) NOT NULL,
+      condition VARCHAR(255) NOT NULL,
+      location VARCHAR(255) NOT NULL,
+      demand VARCHAR(255) NOT NULL,
+      user_id INT REFERENCES users(id),
+      watch_count INT NOT NULL,
+      view_count INT NOT NULL,
+      offer_count INT NOT NULL,
+      status VARCHAR(255) NOT NULL
       )
       `
     );
     console.log('successfully created users table');
   } catch (err) {
-    console.log('error creating users table ', err)
+    console.log('error creating users table ', err);
   }
 };
 
 export const dropPostTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS posts`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS posts`);
     console.log('successfully dropped posts table');
   } catch (err) {
     console.log('error dropping users table ', err);
@@ -120,28 +105,21 @@ export const createWatchTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS watchs
       (
-      id SERIAL,
-      user_id INTEGER NULL DEFAULT NULL,
-      post_id INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (id),
-      CONSTRAINT fk_watchs_user_id
-      FOREIGN KEY(user_id) REFERENCES users(id),
-      CONSTRAINT fk_watchs_post_id
-      FOREIGN KEY(post_id) REFERENCES posts(id),
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id),
+      post_id INT REFERENCES posts(id)
       )
       `
     );
     console.log('successfully created watchs table');
   } catch (err) {
-    console.log('error creating watchs table ', err)
+    console.log('error creating watchs table ', err);
   }
 };
 
 export const dropWatchTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS watchs`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS watchs`);
     console.log('successfully dropped watchs table');
   } catch (err) {
     console.log('error dropping watchs table ', err);
@@ -154,26 +132,21 @@ export const createRatingTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS ratings
       (
-      id SERIAL,
-      user_id INTEGER NULL DEFAULT NULL,
-      rating INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (id),
-      CONSTRAINT fk_ratings_user_id
-      FOREIGN KEY(user_id) REFERENCES users(id)
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id),
+      rating INT NOT NULL
       )
       `
     );
     console.log('successfully created ratings table');
   } catch (err) {
-    console.log('error creating ratings table ', err)
+    console.log('error creating ratings table ', err);
   }
 };
 
 export const dropRatingTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS watchs`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS watchs`);
     console.log('successfully dropped ratings table');
   } catch (err) {
     console.log('error dropping ratings table ', err);
@@ -186,26 +159,21 @@ export const createPhotoTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS photos
       (
-      id SERIAL,
-      post_id INTEGER NULL DEFAULT NULL,
-      url VARCHAR NULL DEFAULT NULL,
-      PRIMARY KEY (id),
-      CONSTRAINT fk_photos_post_id
-      FOREIGN KEY(post_id) REFERENCES posts(id),
+      id SERIAL PRIMARY KEY,
+      post_id INT REFERENCES posts(id),
+      url VARCHAR(255) NOT NULL
       )
       `
     );
     console.log('successfully created photos table');
   } catch (err) {
-    console.log('error creating photos table ', err)
+    console.log('error creating photos table ', err);
   }
 };
 
 export const dropPhotoTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS photos`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS photos`);
     console.log('successfully dropped photos table');
   } catch (err) {
     console.log('error dropping photos table ', err);
@@ -218,26 +186,21 @@ export const createFollowingTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS followings
       (
-      id SERIAL,
-      user_id INTEGER NULL DEFAULT NULL,
-      follower_id INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (id),
-      CONSTRAINT fk_followings_user_id
-      FOREIGN KEY(user_id) REFERENCES users(id),
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id),
+      follower_id INT NOT NULL
       )
       `
     );
     console.log('successfully created followings table');
   } catch (err) {
-    console.log('error creating followings table ', err)
+    console.log('error creating followings table ', err);
   }
 };
 
 export const dropFollowingTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS followings`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS followings`);
     console.log('successfully dropped followings table');
   } catch (err) {
     console.log('error dropping followings table ', err);
@@ -250,32 +213,26 @@ export const createCategoryTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS categorys
       (
-      id SERIAL,
-      type VARCHAR NULL DEFAULT NULL,
-      post_id INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (id),
-      CONSTRAINT fk_categorys_post_id
-      FOREIGN KEY(post_id) REFERENCES posts(id),
+      id SERIAL PRIMARY KEY,
+      type VARCHAR(255) NOT NULL,
+      post_id INT REFERENCES posts(id)
       )
       `
     );
     console.log('successfully created categorys table');
   } catch (err) {
-    console.log('error creating categorys table ', err)
+    console.log('error creating categorys table ', err);
   }
 };
 
 export const dropCategoryTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS categorys`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS categorys`);
     console.log('successfully dropped categorys table');
   } catch (err) {
     console.log('error dropping categorys table ', err);
   }
 };
-
 
 export const createOfferTable = async () => {
   try {
@@ -283,26 +240,21 @@ export const createOfferTable = async () => {
       `
       CREATE TABLE IF NOT EXISTS offers
       (
-        id SERIAL,
-        buyer_username VARCHAR NULL DEFAULT NULL,
-        post_id INTEGER NULL DEFAULT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_offers_post_id
-        FOREIGN KEY(post_id) REFERENCES posts(id),
+        id SERIAL PRIMARY KEY,
+        buyer_username VARCHAR(18) NOT NULL,
+        post_id INT REFERENCES posts(id)
       )
       `
     );
     console.log('successfully created offers table');
   } catch (err) {
-    console.log('error creating offers table ', err)
+    console.log('error creating offers table ', err);
   }
 };
 
 export const dropOfferTable = async () => {
   try {
-    await db.queryAsync(
-      `DROP TABLE IF EXISTS offers`
-    );
+    await db.queryAsync(`DROP TABLE IF EXISTS offers`);
     console.log('successfully dropped offers table');
   } catch (err) {
     console.log('error dropping offers table ', err);
