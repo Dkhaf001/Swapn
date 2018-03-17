b7c115ba51df65bdeaafc51fd8c024986ca2ac1b
qiang song <qiangsong889@yahoo.com>
2018-03-17 14:04:32 -0700

    added redux to frond end

--- thesis-ui/client/src/index.jsx
+++ thesis-ui/client/src/index.jsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// import Sling from './components/Sling/index.jsx';
-// import Auth from './components/Auth/Signup.jsx';
 import App from './App.jsx';
+import store from './store'
 
 import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store ={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
   ,document.getElementById('app'),
 );
---
+++ thesis-ui/client/src/store/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+
+import dataReducers from './dataReducers'
+
+export default combineReducers({
+  dataReducers
+})
