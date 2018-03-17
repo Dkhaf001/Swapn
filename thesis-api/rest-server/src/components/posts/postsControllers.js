7b1b63a17f10dbc2b3161d476a6758cf55191ddf
Daniel Khafi <Dkhaf001@gmail.com>
2018-03-16 19:58:37 -0700

    styles

--- thesis-api/rest-server/src/components/posts/postsControllers.js
+++ thesis-api/rest-server/src/components/posts/postsControllers.js
@@ -1,14 +1,50 @@
-export const Controller = async (req, res) => {
+import {
+  fetchAllPostsHelper,
+  fetchUserPostsHelper,
+  fetchSinglePostsHelper,
+  addPostsHelper
+} from './postsSQLHelpers.js';
+import db from '../../config/database/index';
+
+export const fetchUserPostsController = async (req, res) => {
+  let payload;
+  payload = req.params;
   try {
-    
+    const data = await fetchUserPostsHelper(payload);
+    return res.status(200).send(data);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 };
 
-export const Controller = async (req, res) => {
+export const fetchAllPostsController = async (req, res) => {
+  let payload;
+  payload = req.params;
   try {
+    const data = await fetchAllPostsHelper(payload);
+    return res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+export const fetchSinglePostsController = async (req, res) => {
+  let payload;
+  payload = req.params;
+  try {
+    const data = await fetchSinglePostsHelper(payload);
+    return res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const addPostsController = async (req, res) => {
+  let payload;
+  payload = req.params;
+  try {
+    const data = await addPostsHelper(payload);
+    return res.status(200).send(data);
   } catch (err) {
     console.log(err);
   }
--- thesis-api/rest-server/src/components/posts/postsQueries.js
+++ thesis-api/rest-server/src/components/posts/postsQueries.js
@@ -1,14 +1,52 @@
-export const Query = async (req, res) => {
+import db from '../../config/database';
+import {
+  fetchPostsHelper,
+  fetchAllPostsHelper,
+  fetchSinglePostHelper,
+  addPostsHelper
+} from './postsSQLHelpers';
+
+export const fetchPostsQueryHelper = async (req, res) => {
   try {
-    
+    const queryString = fetchPostsHelper(payload);
+    const data = await db.queryAsync(queryString);
+    console.log('fetchPostsQueryHelper - successfully retrieved data', data);
+    return data;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 };
 
-export const Query = async (req, res) => {
+export const fetchAllPostsQueryHelper = async (req, res) => {
   try {
+    const queryString = fetchAllPostsHelper(payload);
+    const data = await db.queryAsync(queryString);
+    console.log('fetchAllPostsQueryHelper - successfully retrieved data', data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const fetchSinglePostsQueryHelper = async (req, res) => {
+  try {
+    const queryString = fetchSinglePostsHelper(payload);
+    const data = await db.queryAsync(queryString);
+    console.log(
+      'fetchSinglePostsQueryHelper - successfully retrieved data',
+      data
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+export const addPostsQueryHelper = async (req, res) => {
+  try {
+    const queryString = addPostsHelper(payload);
+    const data = await db.queryAsync(queryString);
+    console.log('addPostsQueryHelper - successfully retrieved data', data);
+    return data;
   } catch (err) {
     console.log(err);
   }
