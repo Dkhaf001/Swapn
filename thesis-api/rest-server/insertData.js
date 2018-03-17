b8b01a1a7266b97724c99b23522f49d3317b9fbe
Daniel Khafi <Dkhaf001@gmail.com>
2018-03-16 17:44:49 -0700

    frontend styles

--- thesis-api/rest-server/insertData.js
+++ thesis-api/rest-server/insertData.js
@@ -1,25 +1,16 @@
-const Classes = require("./db/models/classes.js");
-const Lectures = require("./db/models/lectures.js");
-const Quizzes = require("./db/models/quizzes.js");
-const Schools = require("./db/models/schools.js");
-const Students = require("./db/models/students.js");
-const StudentsClasses = require("./db/models/studentsClasses.js");
-const StudentsQuizzes = require("./db/models/studentsQuizzes.js");
-const StudentsTeachers = require("./db/models/studentsTeachers.js");
-const Teachers = require("./db/models/teachers.js");
-const Topics = require("./db/models/topics.js");
+const categoriesData = require("./src/lib/data/categories.json");
+const followingsData = require("./src/lib/data/following.json");
+const offersData = require("./src/lib/data/offer.json");
+const postsData = require("./src/lib/data/posts.json");
+const usersData = require("./src/lib/data/users.json");
+const watchersData = require("./src/lib/data/watchers.json");
 
-const categoriesData = require("./db/data/classes.json");
-const followingData = require("./db/data/lectures.json");
-const offerData = require("./db/data/quizzes.json");
-const postsData = require("./db/data/schools.json");
-const usersData = require("./db/data/students.json");
-const watchersClassesData = require("./db/data/studentsClasses.json");
+const Tables = require('./src/lib/SQL/index.js');
 
 
 const create = async () => {
   try {
-    await Schools.bulkCreate(schoolsData);
+    await Categorys.bulkCreate(categoriesData);
     await Teachers.bulkCreate(teachersData);
     await Classes.bulkCreate(classesData);
     await Lectures.bulkCreate(lecturesData);
--- thesis-ui/client/src/components/Navbar/index.jsx
+++ thesis-ui/client/src/components/Navbar/index.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-
+import Search from './Search.jsx';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 class Navbar extends Component {
   constructor() {
     super()
   }
   render() {
     return (
-      <div>
-        Hello from Navbar
+      <div className="rowbanner">
+          <div className="leftbanner">{this.props.user ? `Current User: ${this.props.user}` : ''}</div>
+          {console.log(this.props.user)}
+          <div className="middlebanner">
+          <Search />
+          <Link to="/auth" className="linkbutton"> Login </Link>
+          <Link to="/post" className="linkbutton"> List </Link>
+          <Link to="/messages" className="linkbutton"> Messages </Link>
+          {/* <LoginLogout onClick={this.props.onClick} authed={this.props.auth} user={this.props.user} type={this.props.type} /> */}
+          {/* <Link to="/login" className="linkbutton"> Login </Link>
+          <button className="linkbutton" name="logout" onClick={this.props.onClick}> Logout </button> */}
+          </div>
+          
+      
       </div>
     )
   }
