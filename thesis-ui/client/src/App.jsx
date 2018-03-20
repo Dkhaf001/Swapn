3d105643af30c841072e342e1e2537e90e2a385b
Daniel Khafi <Dkhaf001@gmail.com>
2018-03-19 17:24:02 -0700

    rebase

--- thesis-ui/client/src/App.jsx
+++ thesis-ui/client/src/App.jsx
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-// import React, { Component } from "react";
-// import { connect } from 'react-redux'
-// import { bindActionCreators } from 'redux'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   Switch,
-//   Redirect
-// } from "react-router-dom";
-// import View from "./components/View/view.jsx";
-// import Navbar from "./components/Navbar/index.jsx";
-// import { addDataToStore } from './actions'
-=======
-import React, { Component } from "react";
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import View from "./components/View/view.jsx";
-import Navbar from "./components/Navbar/index.jsx";
-import { addDataToStore } from './actions'
-import HomePostList from "./components/Feed/homePostList";
->>>>>>> redux work
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       inputValue: ''
-//     }
-//   }
-//   handleSubmit() {
-//     this.props.addDataToStore(this.state.inputValue)
-//   }
-//   render() {
-//     return (
-//       <Router>
-//         <div>
-//           <Navbar />
-//           <View />
-//           <input type='text' onChange={(e) => this.setState({inputValue: e.target.value})} />
-//           <input type='submit' onClick={()=>this.props.addDataToStore(this.state.inputValue)}/>
-//           {this.props.dataFromReduxStorage}
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
-// function mapStateToProps(state) {
-//   return {
-//     dataFromReduxStorage: state.dataReducers
-//   }
-// }
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators({
-//     addDataToStore
-//   }, dispatch)
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -87,7 +19,6 @@ class App extends React.Component {
   }
   render() {
     return (
-<<<<<<< HEAD
       <div>
         <Navbar />
         <div ref="mainPanel">
@@ -101,16 +32,6 @@ class App extends React.Component {
               })}
             </Switch>
           </div>
-=======
-      <Router>
-        <div>
-          <Navbar />
-          <HomePostList />
-          {/* <View /> */}
-          <input type='text' onChange={(e) => this.setState({inputValue: e.target.value})} />
-          <input type='submit' onClick={()=>this.props.addDataToStore(this.state.inputValue)}/>
-          {this.props.dataFromReduxStorage}
->>>>>>> redux work
         </div>
       </div>
     );
