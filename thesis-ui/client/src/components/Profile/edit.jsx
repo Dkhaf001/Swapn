09503b712316c0de0a2479946f970966e56bb334
Elbert Chao <elbertchao@gmail.com>
2018-03-19 22:21:05 -0700

    nav var code clean up, and started profile rendering

--- thesis-ui/client/src/components/Profile/edit.jsx
+++ thesis-ui/client/src/components/Profile/edit.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-//this should be a popup that allows the user to update his profile details and profile pic
+// this should be a popup that allows the user to update his profile details and profile pic
 class Edit extends Component {
   constructor() {
     super();
   }
+  // need two imput feild set gut request with axios when submit// needs photo upload
+
   render() {
     return <div>Hello from Edit</div>;
   }
