9ac3dc5c958eac1ad72656c8f4132befc6ee0040
Elbert Chao <elbertchao@gmail.com>
2018-03-21 15:03:12 -0700

    nested routes work

--- thesis-ui/client/src/routes/app.jsx
+++ thesis-ui/client/src/routes/app.jsx
@@ -4,6 +4,11 @@ import Profile from '../components/Profile/sellerProfile.jsx';
 import SignUp from '../components/Auth/Signup.jsx';
 import Login from '../components/Auth/Login.jsx';
 import Home from '../components/Feed/homePostList.jsx';
+import SellingList from '../components/Feed/sellersPostList.jsx';
+import WatchingList from '../components/Feed/watchingPostList.jsx';
+import BarteringList from '../components/Feed/barteringsPostList.jsx';
+import ListingList from '../components/Feed/listingPostList.jsx';
+import FollowingList from '../components/Following/index.jsx';
 
 const Rt = {};
 Rt.appRoutes = [
@@ -19,12 +24,6 @@ Rt.appRoutes = [
     sidebarName: 'Profile',
     navbarName: 'Profile',
     component: Profile,
-  },
-  {
-    path: '/profile/selling',
-    sidebarName: 'Selling',
-    navbarName: 'Selling',
-    component: Edit,
   },
   {
     path: '/post',
@@ -57,25 +56,25 @@ Rt.profileRoutes = [
     path: '/profile/following',
     sidebarName: 'Following',
     navbarName: 'Following',
-    component: Home,
+    component: FollowingList,
   },
   {
     path: '/profile/watchlist',
     sidebarName: 'Watchlist',
     navbarName: 'Watchlist',
-    component: Home,
+    component: WatchingList,
   },
   {
     path: '/profile/bartering',
     sidebarName: 'Bartering',
     navbarName: 'Bartering',
-    component: Home,
+    component: BarteringList,
   },
   {
     path: '/profile/selling',
     sidebarName: 'Selling',
     navbarName: 'Selling',
-    component: Home,
+    component: SellingList,
   },
 ];
 
