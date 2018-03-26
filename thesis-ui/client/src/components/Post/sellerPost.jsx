861214386f80c1f52fdb58265943ef1ff4d18344
Edward Roh <edroh90@gmail.com>
2018-03-26 11:52:58 -0700

    [ui] refactored complete transaction with conditional rendering.. it will only show if status is pending. implemented relist button which shows when status is bartered

--- thesis-ui/client/src/components/Post/sellerPost.jsx
+++ thesis-ui/client/src/components/Post/sellerPost.jsx
@@ -4,14 +4,6 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
 import EditPost from './editPost.jsx';
-// edit post should reopen addPost but with the info already filled out including photos
-// should render chats if there are ongoing offers
-
-// COMMENTS FROM EDDIE
-// this component renders when the SELLER goes to their POST
-// - need to link in follows
-// - ADD more images
-// - COMPLETED TRANSACTION = button that marks item as SOLD
 
 class SellerPost extends Component {
   constructor(props) {
@@ -27,12 +19,13 @@ class SellerPost extends Component {
         demand: this.props.current_post.demand,
         status: this.props.current_post.status,
         main_photo: this.props.current_post.main_photo
-      }
+      },
+      sold: false
     };
   }
 
   async componentWillMount() {
-    this.getPhotos();
+    // this.getPhotos();
     if (this.props.current_post.status === 'Accepting Offers') {
       this.setState({
         offerAccepted: false
@@ -41,28 +34,23 @@ class SellerPost extends Component {
       this.setState({
         offerAccepted: true
       });
+    } else if (this.props.current_post.status === 'BARTERED') {
+      this.setState({
+        offerAccepted: true,
+        sold: true
+      });
     }
   }
 
-  async getPhotos() {
-    // const postId = this.props.current_posts.id;
-    // const { data } = await axios.get(
-    //   `http://localhost:3396/api/photos/${postId}`
-    // );
-    // console.log('successfully received photos!');
-    // this.setState({
-    //   photos: data.rows
-    // });
-  }
-
-  // async removePost() {
-  //   const userId = this.props.current_post.user_id;
-  //   const postId = this.props.current_post.id;
-  //   const { data } = await axios.delete(
-  //     `http://localhost:3396/api/photos/${userId}/${postId}`
-  //   );
-  //   console.log('successfully deleted post!');
-  //   // refresh page after successfully deleting?
+  // async getPhotos() {
+  // const postId = this.props.current_posts.id;
+  // const { data } = await axios.get(
+  //   `http://localhost:3396/api/photos/${postId}`
+  // );
+  // console.log('successfully received photos!');
+  // this.setState({
+  //   photos: data.rows
+  // });
   // }
 
   editPost() {
@@ -115,13 +103,45 @@ class SellerPost extends Component {
       );
       this.setState({
         offerAccepted: false,
-        accept: cancel
+        accept: cancel,
+        sold: false
       });
       console.log(
         'Successfully cancelled an offer! Post status is now Accepting Offers'
       );
     } catch (err) {
       console.log('Error cancelling offer!');
+    }
+  }
+
+  async soldOffer() {
+    try {
+      let sold = {
+        title: this.props.current_post.title,
+        description: this.props.current_post.description,
+        condition: this.props.current_post.condition,
+        location: this.props.current_post.location,
+        demand: this.props.current_post.demand,
+        status: 'BARTERED',
+        main_photo: this.props.current_post.main_photo
+      };
+      let userId = this.props.current_post.user_id;
+      let postId = this.props.current_post.id;
+      const data = await axios.put(
+        `http://localhost:3396/api/posts/${userId}/${postId}`,
+        sold
+      );
+      this.setState({
+        offerAccepted: false,
+        accept: sold,
+        sold: true
+      });
+      console.log(
+        'Successfully bartered an offer! Post status is now BARTERED'
+      );
+      this.props.history.push('/home');
+    } catch (err) {
+      console.log('Error completing barter transaction!');
     }
   }
 
@@ -145,19 +165,22 @@ class SellerPost extends Component {
           </h4>
           <h4>Status: {this.state.accept.status}</h4>
         </div>
-        <RaisedButton
-          label="Edit Post"
-          primary={true}
-          style={{ margin: 12 }}
-          onClick={() => this.editPost()}
-        />
-        <RaisedButton
-          label="Complete Transaction"
-          secondary={true}
-          style={{ margin: 12 }}
-          // this onClick event should update the STATUS to SOLD
-          // onClick={() => this.removePost()}
-        />
+        {this.state.sold === false ? (
+          <RaisedButton
+            label="Edit Post"
+            primary={true}
+            style={{ margin: 12 }}
+            onClick={() => this.editPost()}
+          />
+        ) : null}
+        {this.state.sold === false && this.state.offerAccepted === true ? (
+          <RaisedButton
+            label="Complete Transaction"
+            secondary={true}
+            style={{ margin: 12 }}
+            onClick={() => this.soldOffer()}
+          />
+        ) : null}
         <br />
         {this.state.offerAccepted === false ? (
           <RaisedButton
@@ -166,9 +189,16 @@ class SellerPost extends Component {
             style={{ margin: 12 }}
             onClick={() => this.acceptOffer()}
           />
-        ) : (
+        ) : this.state.sold === false ? (
           <RaisedButton
             label="Cancel Offer"
+            secondary={true}
+            style={{ margin: 12 }}
+            onClick={() => this.cancelOffer()}
+          />
+        ) : (
+          <RaisedButton
+            label="Relist"
             secondary={true}
             style={{ margin: 12 }}
             onClick={() => this.cancelOffer()}
