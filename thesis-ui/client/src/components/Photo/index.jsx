3962f0fae445cc9a9768771a0f457f3f0bf593a2
Elbert Chao <elbertchao@gmail.com>
2018-03-26 17:28:33 -0700

    test photo render

--- thesis-ui/client/src/components/Photo/index.jsx
+++ thesis-ui/client/src/components/Photo/index.jsx
@@ -36,45 +36,52 @@ class PhotoUpload extends React.Component {
       file: null,
     });
   };
-  handleUpload = async () => {
+  handleUpload = async (postId) => {
     // this.props.loadingTrue();
-    console.log('clicked');
     const value = this.state.val;
 
     const { file } = this.state;
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('post_id', 1); // this is from state post id
+    formData.append('post_id', postId); // this is from state post id
     // console.log('formData', formData);
     // change to match the route i need for dp route
 
     // 1 changes to postID
-    await axios.post('http://localhost:8593/api/addphoto/1', formData);
+    await axios.post(`http://localhost:8593/api/addphoto/${postId}`, formData);
     this.setState({
       file: null,
     });
   };
 
-  removePhoto = async (album, key) => {
+  removePhoto = async (postId, key) => {
     try {
-      const data = await axios.delete(`http://localhost:8593/api/removephoto/${album}/${key}`);
+      const data = await axios.delete(`http://localhost:8593/api/removephoto/${postId}/${key}`);
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  fetchAlbum = async () => {
+  fetchAlbum = async (postId) => {
     try {
-      const data = await axios.get('http://localhost:8593/api/1');
+      const data = await axios.get(`http://localhost:8593/api/${postId}`);
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
+
+  // photos = (bucket) => {
+  // <img src=`https://barterbruh.s3.amazonaws.com/${key}` /> ;
+  // };
 
   renderForm = () => (
     <div>
+      <img
+        style={{ width: '128px', height: '128px' }}
+        src={'https://s3-us-west-1.amazonaws.com/barterbruh/1/1de93ec.jpg'}
+      />
       <form>
         <label>
           Upload:
