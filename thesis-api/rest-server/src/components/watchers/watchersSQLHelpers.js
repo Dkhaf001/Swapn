48b58e30bfb36acf43c8bdbf3b6dbf830636b2a3
Elbert Chao <elbertchao@gmail.com>
2018-03-29 19:24:36 -0700

    fixed watchlist

--- thesis-api/rest-server/src/components/watchers/watchersSQLHelpers.js
+++ thesis-api/rest-server/src/components/watchers/watchersSQLHelpers.js
@@ -1,34 +1,26 @@
-export const addWatchesHelper = ({ user_id, post_id }) => {
-  return `
+export const addWatchesHelper = ({ user_id, post_id }) => `
   INSERT INTO watchs (user_id, post_id)
   VALUES (${user_id}, ${post_id})
   RETURNING *
   `;
-};
 
-export const removeWatchesHelper = ({ user_id, post_id }) => {
-  return `
+export const removeWatchesHelper = ({ user_id, post_id }) => `
   DELETE FROM watchs
   WHERE post_id=${post_id} AND user_id=${user_id}
   `;
-};
 
-export const fetchAllWatchesHelper = ({ user_id }) => {
-  return `
+export const fetchAllWatchesHelper = ({ user_id }) => `
   SELECT * 
-  FROM posts 
-  INNER JOIN watchs
+  FROM watchs 
+  INNER JOIN posts
   ON posts.id = watchs.post_id 
   WHERE watchs.user_id=${user_id}
   `;
-};
 
-export const fetchSingleWatchesHelper = ({ user_id, post_id }) => {
-  return `
+export const fetchSingleWatchesHelper = ({ user_id, post_id }) => `
   SELECT * 
-  FROM posts 
-  INNER JOIN watchs
+  FROM watchs 
+  INNER JOIN posts
   ON posts.id = watchs.post_id 
   WHERE watchs.user_id=${user_id} AND watchs.post_id=${post_id}
   `;
-};
