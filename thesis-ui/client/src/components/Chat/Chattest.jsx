3205e983a4bbfb469528c9f93dfbfcf9c13cb5a5
Daniel Khafi <Dkhaf001@gmail.com>
2018-04-05 17:00:14 -0700

    quick change

--- thesis-ui/client/src/components/Chat/Chattest.jsx
+++ thesis-ui/client/src/components/Chat/Chattest.jsx
@@ -107,13 +107,13 @@ class Chattest extends React.Component {
         </button>
         <div className={this.state.modal ? 'modal-active' : 'modal'} id="modal-id">
           <a href="#close" class="modal-overlay" aria-label="Close" />
-          <div class="modal-container">
-            <div class="modal-header">
+          <div className="modal-container">
+            <div className="modal-header">
               <a href="#close" class="btn btn-clear float-right" aria-label="Close" />
-              <div class="modal-title h5">Chat</div>
+              <div className="modal-title h5">Chat</div>
             </div>
-            <div class="modal-body">
-              <div class="content">
+            <div className="modal-body">
+              <div className="content">
                 <div className="chatBox" id={this.props.roomId}>
                   <div className="chat">
                     {this.state.messages.map((message, key) => (
